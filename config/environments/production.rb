49c0f08d1024e69df6ddaa5b722b29461b8efd18
Erik Larsson <eriklarsson@gmail.com>
2013-10-17 22:50:09 +0200

    Fixed Paperclip S3 configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,8 +71,9 @@ Omrails::Application.configure do
   # Configuring Amazon S3 for Paperclip file uploads
   config.paperclip_defaults = {
     :storage => :s3,
+    :s3_protocol => 'http',
+    :bucket => ENV['S3_BUCKET_NAME'],
     :s3_credentials => {
-      :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
